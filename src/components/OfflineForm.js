22f4117db3ce89a639b36cdd4719f5aaff7edeac
whirmill <lorenzo@bemimd.me>
2018-10-15 18:19:40 +0200

    removed title from offline form once the message has been sent

--- src/components/OfflineForm.js
+++ src/components/OfflineForm.js
@@ -86,7 +86,7 @@ class OfflineForm extends Component {
   render() {
     return (
       <CardContainer
-        title={this.props.title}
+        title={!this.state.sent && this.props.title}
         addClass="offline-card"
         contentAddClass={this.state.sent ? 'sent' : ''}
         icon={<MessageSvg />}
