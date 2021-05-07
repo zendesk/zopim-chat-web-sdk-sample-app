1ff121c39e3ab63b18373340ac6053071f2242d3
Nareg Aslanian <nareg@exodus.io>
2021-05-07 12:53:37 +0300

    add account key

--- src/config/base.js
+++ src/config/base.js
@@ -2,7 +2,7 @@
 
 // Settings configured here will be merged into the final config object.
 export default {
-	ACCOUNT_KEY: '',
+	ACCOUNT_KEY: '1eAXd7HIi7sucddLCtV4lRVuQA4TNss4',
 	// Set to 'docked' or 'normal' for docked or normal mode respectively
 	THEME: 'normal'
 }
